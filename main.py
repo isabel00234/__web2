134115b5d4092d60cbef5bb637cc29686ca7eb1d
isabel <isabellin22@yahoo.com.tw>
2022-12-24 15:56:48 +0800

    modify

--- main.py
+++ main.py
@@ -9,3 +9,11 @@ def index():
 @app.route('/name/<username>')
 def show_user(username):
     return f"<h3>您的姓名是<strong>{username}</strong></h3>"
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == "POST":
+        print('由表單傳送過來的')
+        print(f"email:{request.form['email']}")
+        print(f"password:{request.form['password']}")
+    return render_template('login.html')
